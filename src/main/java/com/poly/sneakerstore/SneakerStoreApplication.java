fa534f11a3a84f64a6113f28f07f03bded5dbb4d
lamdayne <nlam32428@gmail.com>
2026-03-13 07:40:51 +0700

    init project

---
+++ src/main/java/com/poly/sneakerstore/SneakerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.poly.sneakerstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakerStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poly/sneakerstore/SneakerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.sneakerstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
