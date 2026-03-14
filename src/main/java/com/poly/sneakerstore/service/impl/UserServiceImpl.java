20979e8fb6b668748a2c6e463a064e2d2c745b98
lamdayne <nlam32428@gmail.com>
2026-03-14 22:29:46 +0700

    feat(service): add interface UserService and implements CRUD method for User

---
+++ src/main/java/com/poly/sneakerstore/service/impl/UserServiceImpl.java
@@ -0,0 +1,73 @@
+package com.poly.sneakerstore.service.impl;
+
+import com.poly.sneakerstore.dto.request.CreateUserRequest;
+import com.poly.sneakerstore.dto.request.UpdateUserRequest;
+import com.poly.sneakerstore.dto.response.UserResponse;
+import com.poly.sneakerstore.exception.AppException;
+import com.poly.sneakerstore.exception.ErrorCode;
+import com.poly.sneakerstore.mapper.UserMapper;
+import com.poly.sneakerstore.model.Role;
+import com.poly.sneakerstore.model.User;
+import com.poly.sneakerstore.repository.UserRepository;
+import com.poly.sneakerstore.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
+
+    @Override
+    public UserResponse createUser(CreateUserRequest request) {
+        User user = userMapper.toUser(request);
+
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new AppException(ErrorCode.EMAIL_EXISTS);
+        } else if (userRepository.existsByPhone(user.getPhone())) {
+            throw new AppException(ErrorCode.PHONE_EXISTS);
+        }
+
+        user.setAvatarUrl("https://files.catbox.moe/lonccy.jpg"); // AVATAR DEFAULT
+        user.setActive(true);
+        user.setRole(Role.USER);
+        user.setCreatedAt(LocalDateTime.now());
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+    @Override
+    public UserResponse updateUser(String userId, UpdateUserRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+
+        userMapper.updateUser(user, request);
+        user.setUpdatedAt(LocalDateTime.now());
+
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        user.setActive(false);
+        userRepository.save(user);
+    }
+
+    @Override
+    public UserResponse getUserById(String userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        return userMapper.toUserResponse(user);
+    }
+
+    @Override
+    public List<UserResponse> getAllUsers() {
+        return userMapper.toUserResponseList(userRepository.findAll());
+    }
+}
