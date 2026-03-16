b540e01c0df787cc3afadc4745ae6301280bee7d
lamdayne <nlam32428@gmail.com>
2026-03-16 18:52:37 +0700

    feat(model): add Address entity

---
+++ src/main/java/com/poly/sneakerstore/model/Address.java
@@ -0,0 +1,29 @@
+package com.poly.sneakerstore.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "addresses")
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String recipientName;
+    private String phone;
+    private String province;
+    private String district;
+    private String ward;
+    private String streetAddress;
+    @Column(name = "is_default")
+    private boolean defaultAddress;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
