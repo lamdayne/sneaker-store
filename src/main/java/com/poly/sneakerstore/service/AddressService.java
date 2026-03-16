16b78b347b685aad00b89b857d7a3e57e0b431ef
lamdayne <nlam32428@gmail.com>
2026-03-16 18:54:15 +0700

    feat(service): add interface and implement AddressService

---
+++ src/main/java/com/poly/sneakerstore/service/AddressService.java
@@ -0,0 +1,16 @@
+package com.poly.sneakerstore.service;
+
+import com.poly.sneakerstore.dto.request.CreateAddressRequest;
+import com.poly.sneakerstore.dto.request.UpdateAddressRequest;
+import com.poly.sneakerstore.dto.response.AddressResponse;
+
+import java.util.List;
+
+public interface AddressService {
+    AddressResponse createAddress(CreateAddressRequest request);
+    AddressResponse updateAddress(String addressId, UpdateAddressRequest request);
+    void deleteAddress(String addressId);
+    AddressResponse getAddressById(String addressId);
+    List<AddressResponse> getAllAddress();
+    void changeAddressDefault(String addressId, String userId);
+}
