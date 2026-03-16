d8987c28a742b4604c4c0cc8dbcdd1c2c456062d
lamdayne <nlam32428@gmail.com>
2026-03-16 18:47:42 +0700

    feat(exception): add ErrorCode for Address

--- src/main/java/com/poly/sneakerstore/exception/ErrorCode.java
+++ src/main/java/com/poly/sneakerstore/exception/ErrorCode.java
@@ -15,6 +15,12 @@ public enum ErrorCode {
     PASSWORD_INVALID(1005, "password has must be greater than 8 character", HttpStatus.BAD_REQUEST),
     EMAIL_EXISTS(1006, "email already exists", HttpStatus.BAD_REQUEST),
     PHONE_EXISTS(1007, "phone already exists", HttpStatus.BAD_REQUEST),
+    RECIPIENT_NAME_NOT_BLANK(1008, "recipientName can not blank", HttpStatus.BAD_REQUEST),
+    PROVINCE_NOT_BLANK(1009, "province can not blank", HttpStatus.BAD_REQUEST),
+    DISTRICT_NOT_BLANK(10010, "district can not blank", HttpStatus.BAD_REQUEST),
+    WARD_NOT_BLANK(10011, "ward can not blank", HttpStatus.BAD_REQUEST),
+    STREET_ADDRESS_NOT_BLANK(10012, "streetAddress can not blank", HttpStatus.BAD_REQUEST),
+    ADDRESS_NOT_FOUND(10013, "Address not found", HttpStatus.NOT_FOUND),
     ;
     private int status;
     private String message;
