594d0c77a02f8b704452e7463f672899cfc5f37b
lamdayne <nlam32428@gmail.com>
2026-03-19 07:29:13 +0700

    feat(repository): add Brand repository

---
+++ src/main/java/com/poly/sneakerstore/repository/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.poly.sneakerstore.repository;
+
+import com.poly.sneakerstore.model.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, String> {
+}
