d30c7aadf693aaf7211594160728d47e2b5b0cf6
lamdayne <nlam32428@gmail.com>
2026-03-19 07:14:19 +0700

    feat(exception): add ErrorCode for Category and Brand

--- src/main/java/com/poly/sneakerstore/exception/ErrorCode.java
+++ src/main/java/com/poly/sneakerstore/exception/ErrorCode.java
@@ -21,6 +21,15 @@ public enum ErrorCode {
     WARD_NOT_BLANK(10011, "ward can not blank", HttpStatus.BAD_REQUEST),
     STREET_ADDRESS_NOT_BLANK(10012, "streetAddress can not blank", HttpStatus.BAD_REQUEST),
     ADDRESS_NOT_FOUND(10013, "Address not found", HttpStatus.NOT_FOUND),
+    CATEGORY_NOT_FOUND(10014, "Category not found", HttpStatus.NOT_FOUND),
+    CATEGORY_NAME_NOT_BLANK(10015, "Category name can not blank", HttpStatus.BAD_REQUEST),
+    CATEGORY_IMG_NOT_BLANK(10016, "Category image can not blank", HttpStatus.BAD_REQUEST),
+    DISPLAY_ORDER(10017, "Display order must be greater than or equal to 1", HttpStatus.BAD_REQUEST),
+    ACTIVE_NOT_NULL(10028, "Active not null", HttpStatus.BAD_REQUEST),
+    BRAND_NOT_FOUND(10029, "Brand not found", HttpStatus.NOT_FOUND),
+    BRAND_NAME_NOT_BLANK(10030, "Brand name can not blank", HttpStatus.BAD_REQUEST),
+    BRAND_LOGO_NOT_BLANK(10031, "Brand logo can not blank", HttpStatus.BAD_REQUEST),
+    BRAND_DESC_NOT_BLANK(10032, "Brand description can not blank", HttpStatus.BAD_REQUEST),
     ;
     private int status;
     private String message;
