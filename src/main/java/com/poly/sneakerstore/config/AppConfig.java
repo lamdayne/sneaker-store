6bfe5c7a7c0bce73c358f80159d53bd51fcaa208
lamdayne <nlam32428@gmail.com>
2026-03-19 07:34:22 +0700

    feat(config): add cors config

---
+++ src/main/java/com/poly/sneakerstore/config/AppConfig.java
@@ -0,0 +1,27 @@
+package com.poly.sneakerstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5500/")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
