be636ccfb2046aeca90857f3ce68d3bf11db596b
lamdayne <nlam32428@gmail.com>
2026-04-06 11:05:16 +0700

    feat(cart_item): refactor all cartitem

--- src/main/java/com/poly/sneakerstore/dto/request/AddCartItemRequest.java
+++ src/main/java/com/poly/sneakerstore/dto/request/AddCartItemRequest.java
@@ -2,6 +2,7 @@ package com.poly.sneakerstore.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Data
@@ -10,12 +11,23 @@ import lombok.*;
 @AllArgsConstructor
 public class AddCartItemRequest {
 
-    @NotBlank(message = "CART_ID_NOT_BLANK")
-    private String cartId;
+    @NotBlank(message = "USER_ID_NOT_BLANK")
+    private String userId;
 
-    @NotBlank(message = "VARIANT_ID_NOT_BLANK")
-    private String variantId;
+    @NotBlank(message = "PRODUCT_ID_NOT_BLANK")
+    private String productId;
 
+    @NotBlank(message = "COLOR_NOT_BLANK")
+    private String color;
+
+    @NotBlank(message = "SIZE_NOT_BLANK")
+    private String size;
+
+    @NotNull(message = "QUANTITY_NOT_NULL")
     @Min(value = 1, message = "QUANTITY_INVALID")
-    private int quantity;
+    private Integer quantity;
+
+    @NotNull(message = "UNIT_PRICE_NOT_NULL")
+    @Min(value = 1, message = "UNIT_PRICE_INVALID")
+    private Double unitPrice;
 }
