717bc5708f797a9fd1558203ef4fdc2be242a904
lamdayne <nlam32428@gmail.com>
2026-04-06 17:21:13 +0700

    feat(authentication): update authentication service

--- src/main/java/com/poly/sneakerstore/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/poly/sneakerstore/service/impl/AuthenticationServiceImpl.java
@@ -2,12 +2,20 @@ package com.poly.sneakerstore.service.impl;
 
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import com.poly.sneakerstore.dto.request.AuthenticationRequest;
+import com.poly.sneakerstore.dto.request.IntrospectRequest;
+import com.poly.sneakerstore.dto.request.LogoutRequest;
+import com.poly.sneakerstore.dto.request.RefreshTokenRequest;
 import com.poly.sneakerstore.dto.response.AuthenticationResponse;
+import com.poly.sneakerstore.dto.response.IntrospectResponse;
 import com.poly.sneakerstore.exception.AppException;
 import com.poly.sneakerstore.exception.ErrorCode;
+import com.poly.sneakerstore.model.InvalidatedToken;
 import com.poly.sneakerstore.model.User;
+import com.poly.sneakerstore.repository.InvalidatedTokenRepository;
 import com.poly.sneakerstore.repository.UserRepository;
 import com.poly.sneakerstore.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +23,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.text.ParseException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.StringJoiner;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -28,6 +39,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final InvalidatedTokenRepository invalidatedTokenRepository;
 
     @Value("${jwt.signer-key}")
     private String signerKey;
@@ -42,15 +54,92 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
         boolean authenticated = passwordEncoder.matches(request.getPassword(), user.getPassword());
 
-        if (!authenticated) throw new AppException(ErrorCode.USER_NOT_FOUND);
+        if (!authenticated) throw new AppException(ErrorCode.UNAUTHENTICATED);
 
         return AuthenticationResponse.builder()
-                .token(generateToken(user))
+                .accessToken(generateAccessToken(user))
+                .refreshToken(generateRefreshToken(user))
                 .authenticated(true)
                 .build();
     }
 
-    private String generateToken(User user) {
+    @Override
+    public IntrospectResponse introspect(IntrospectRequest request) throws ParseException, JOSEException {
+        String token = request.getToken();
+        boolean isValid = true;
+        try {
+            verifyToken(token, false);
+        } catch (AppException e) {
+            isValid = false;
+        }
+        return IntrospectResponse.builder()
+                .valid(isValid)
+                .build();
+    }
+
+
+    @Override
+    public AuthenticationResponse refreshToken(RefreshTokenRequest request) throws ParseException, JOSEException {
+        SignedJWT signedJWT = verifyToken(request.getToken(), true);
+        return null;
+    }
+
+    @Override
+    public void logout(LogoutRequest request) throws ParseException, JOSEException {
+        try {
+            SignedJWT signedJWT = verifyToken(request.getToken(), true);
+
+            String jit = signedJWT.getJWTClaimsSet().getJWTID();
+            Date expiryDate = signedJWT.getJWTClaimsSet().getExpirationTime();
+
+            InvalidatedToken invalidatedToken = InvalidatedToken.builder()
+                    .id(jit)
+                    .expiryTime(expiryDate)
+                    .build();
+
+            invalidatedTokenRepository.save(invalidatedToken);
+        } catch (AppException e) {
+            log.error("Token already expired");
+        }
+    }
+
+    private SignedJWT verifyToken(String token, boolean isRefresh) throws JOSEException, ParseException {
+        JWSVerifier verifier = new MACVerifier(signerKey);
+
+        SignedJWT signedJWT = SignedJWT.parse(token);
+
+        boolean verified = signedJWT.verify(verifier);
+
+        Date expiredTime = isRefresh
+                ? new Date(
+                signedJWT
+                        .getJWTClaimsSet()
+                        .getIssueTime()
+                        .toInstant()
+                        .plus(1, ChronoUnit.HOURS)
+                        .toEpochMilli())
+                : signedJWT.getJWTClaimsSet().getExpirationTime();
+
+        if (!(verified && expiredTime.after(new Date()))) {
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+        }
+
+        if (invalidatedTokenRepository.existsById(signedJWT.getJWTClaimsSet().getJWTID())) {
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+        }
+
+        return signedJWT;
+    }
+
+    private String generateAccessToken(User user) {
+        return generateToken(user, 1);
+    }
+
+    private String generateRefreshToken(User user) {
+        return generateToken(user, 24 * 7);
+    }
+
+    private String generateToken(User user, long time) {
         JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
 
         JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
@@ -58,9 +147,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .issuer("sneaker-store.click")
                 .issueTime(new Date())
                 .expirationTime(new Date(
-                        Instant.now().plus(expiryDuration, ChronoUnit.HOURS).toEpochMilli()
+                        Instant.now().plus(time, ChronoUnit.HOURS).toEpochMilli()
                 ))
-                .claim("scope", buildScope(user.getRole().name()))
+                .jwtID(UUID.randomUUID().toString())
+                .claim("scope", buildScope(user))
                 .build();
 
         Payload payload = new Payload(jwtClaimsSet.toJSONObject());
@@ -76,7 +166,11 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         }
     }
 
-    private String buildScope(String role) {
-        return String.format("ROLE_%s", role);
+    private String buildScope(User user) {
+        StringJoiner scope = new StringJoiner(" ");
+        if (user.getRole().describeConstable().isPresent()) {
+            scope.add(user.getRole().name());
+        }
+        return scope.toString();
     }
 }
